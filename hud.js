f5a86f29c409895a7477e704bbbc23f558530474
asynk <asynk@vk.com>
2024-02-18 00:02:07 +0300

    18.02

--- hud.js
+++ hud.js
@@ -651,3 +651,15 @@ function pad(n, width, z) {
         create_dialog(dId, dType, dHeader, dText, dButton1, dButton2);
         cef.set_focus(true);
     });
+    
+cef.on("show-spectate", () => {
+    document.getElementById("spectate-menu").style = `display: block`;
+});
+
+cef.on("hide-spectate", () => {
+    document.getElementById("spectate-menu").style = `display: none`;
+})
+
+cef.on("update-spectate", (id, value) => {
+    document.getElementById(`spectate-player-${id}`).innerHTML = value;
+});
